52c848753e04b18552d46b9e797737a70e417c29
farah746 <57151262+farah746@users.noreply.github.com>
2020-12-17 22:30:56 +0100

    Update connexion.php

--- php/connexion.php
+++ php/connexion.php
@@ -1,20 +1,22 @@
 <?php
-$url = 'https://tp-erp.odoo.com/';
-$db = 'TP_ERP';
-$username = 'bromdhan746@gmail.com';
-$password = 'admin2020.';
-
-////////////////////////
 
 require_once('ripcord.php');
-$info = ripcord::client('https://tp-erp.odoo.com/start')->start();
-list($url, $db, $username, $password) =
-  array($info['host'], $info['database'], $info['user'], $info['password']);
-  ///////////////////
-  ///////////////////
-  ///////////////////
-  $common  =  ripcord :: client ("$url/xmlrpc/2/common"); 
-  $common -> version ();
 
+
+//Variable
+$url = "http://localhost:8069";
+$db = "TP_ERP";
+$username = 'bromdhan746@gmail.com';
+$password = 'Farah2020.';
+
+echo "$url/xmlrpc/2/common";
+
+
+$common = ripcord::client($url."/xmlrpc/2/common");
+
+$uid = $common->authenticate($db, $username, $password, array());
+echo $uid;
+
+var_dump($uid);
 
 ?>
